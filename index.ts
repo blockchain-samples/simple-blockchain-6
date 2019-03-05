f7fe79ab88e4ad48664235d9838482f577c096f0
SoHye Choi <wergreat10@naver.com>
2019-03-05 15:21:08 +0900

    basic TS example practicce

---
+++ index.ts
@@ -0,0 +1,11 @@
+const name = "kimi",
+    age = 29,
+    gender = "woman";
+
+const sayHi = (name:string, age:number, gender:string):string => {
+    return `Hi ${name}, ${age}, ${gender}`;
+};
+
+console.log(sayHi(name, age, gender));
+
+export {};
