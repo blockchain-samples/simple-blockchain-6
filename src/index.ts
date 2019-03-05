409304700e2d301d8149c9f744d0609313da2417
SoHye Choi <wergreat10@naver.com>
2019-03-05 16:28:37 +0900

    interface practice

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
-const name = "kimi",
-    age = 29,
-    gender = "woman";
+interface Human {
+    name:string;
+    age:number;
+    gender:string;
+}
 
-const sayHi = (name:string, age:number, gender:string):string => {
-    return `Hi ${name}, ${age}, ${gender}`;
+const person = {
+    name: "kimi",
+    age: 29,
+    gender: "woman"
 };
 
-console.log(sayHi(name, age, gender)+'!!');
+const sayHi = (person:Human):string => {
+    return `${person.name}, ${person.age}, ${person.gender}`;
+};
+
+console.log(sayHi(person)+'!!');
 
 export {};
