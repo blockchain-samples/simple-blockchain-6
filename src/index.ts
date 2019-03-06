398cd12b05da1289cedf24cb3535abdef0aff77b
SoHye Choi <wergreat10@naver.com>
2019-03-07 00:32:36 +0900

    add createBlock func

--- src/index.ts
+++ src/index.ts
@@ -31,6 +31,7 @@ class Block {
 }
 
 const genesisBlock: Block = new Block(0, "2020202020", "", "Hello", 201903062031);
+
 let blockChain: Block[] = [genesisBlock];
 
 const getBlockchain = (): Block[] => blockChain;
@@ -39,7 +40,29 @@ const getLatestBlock = (): Block => blockChain[blockChain.length-1];
 
 const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
 
-console.log(blockChain);
+const createNewBlock = (data: string): Block => {
+    const previousBlock: Block = getLatestBlock();
+    const newIndex: number = previousBlock.index+1;
+    const newTimeStamp: number = getNewTimeStamp();
+    const newHash: string = Block.calculateBlockHash(newIndex, previousBlock.hash, data, newTimeStamp);
+
+    const newBlock: Block = new Block(
+        newIndex,
+        newHash,
+        previousBlock.hash,
+        data,
+        newTimeStamp
+    );
+
+    blockChain.push(newBlock);
+
+    return newBlock;
+};
+
+createNewBlock("HI");
+createNewBlock("BYE");
+
+console.log(getBlockchain());
 
 
 export {};
